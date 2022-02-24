173d60579cc08d70d2387114772965fc2e1e71e5
SambaranRepo <sambaranghosal@gmail.com>
2022-02-24 10:44:34 -0800

    WORKING PARTICLE FILTER SLAM

--- code/slam.py
+++ code/slam.py
@@ -38,18 +38,18 @@ class Slam():
         if mode == 1:
             self.k = k
             self.MAP = {}
-            self.MAP['res']   = 0.5 #meters
-            self.MAP['xmin']  = -200 #meters
-            self.MAP['ymin']  = -500
-            self.MAP['xmax']  =  1000
-            self.MAP['ymax']  =  200 #800 previous
+            self.MAP['res']   = 0.75 #meters
+            self.MAP['xmin']  = -1500 #meters
+            self.MAP['ymin']  = -1500
+            self.MAP['xmax']  =  1500
+            self.MAP['ymax']  =  1500 #800 previous
             self.MAP['sizex']  = int(np.ceil((self.MAP['xmax'] - self.MAP['xmin']) / self.MAP['res'] + 1)) #cells
             self.MAP['sizey']  = int(np.ceil((self.MAP['ymax'] - self.MAP['ymin']) / self.MAP['res'] + 1))
             self.MAP['map'] = -np.ones((self.MAP['sizex'],self.MAP['sizey']),dtype=np.int8)
             self.MAP['log_odds'] = np.zeros((self.MAP['sizex'], self.MAP['sizey']))
             self.MAP['free'] = np.zeros((self.MAP['sizex'], self.MAP['sizey']), dtype = np.int8)
-            self.MAP['pose'] = np.zeros((l - l % (self.k * 5),2))
-            self.MAP['traj'] = np.zeros((l - l % (self.k * 5), 3))
+            self.MAP['pose'] = np.zeros((l,2))
+            self.MAP['traj'] = np.zeros((l, 3))
             self.MAP['image'] = np.zeros((self.MAP['sizex'], self.MAP['sizey'],3))
             self.x_im = np.arange(self.MAP['xmin'],self.MAP['xmax']+self.MAP['res'],self.MAP['res'])
             self.y_im = np.arange(self.MAP['ymin'], self.MAP['ymax'] + self.MAP['res'], self.MAP['res'])
@@ -59,8 +59,8 @@ class Slam():
             self.y_range = np.arange(-self.y_max_range, self.y_max_range + self.MAP['res'], self.MAP['res'])
             self.x_mid = int(self.x_max_range / self.MAP['res'])
             self.y_mid = int(self.y_max_range / self.MAP['res'])
-            self.N = 100
-            self.N_threshold = 20
+            self.N = 50
+            self.N_threshold = 5
             self.mu = np.zeros((self.N, 3))
             self.alpha = np.full(self.N, 1 / self.N) 
             self.correlation = np.zeros(self.N,)
@@ -86,7 +86,7 @@ class Slam():
         w_T_robot = np.asarray([[np.cos(theta),-np.sin(theta), 0, sx], [np.sin(theta) , np.cos(theta) , 0 , sy], [0, 0, 1, 0], [0,0,0,1]])
         
         angles = np.linspace(-5,185,286) * np.pi/180
-        valid_index = np.logical_and((ranges < 60),(ranges> 0.1))
+        valid_index = np.logical_and((ranges < 60),(ranges>=0.1))
         ranges = ranges[valid_index]
         angles = angles[valid_index]
         x_lidar = np.cos(angles) * ranges
@@ -100,7 +100,7 @@ class Slam():
         w_y_obj = w_s_obj[1]
         w_z_obj = w_s_obj[2]
         # print(f"Lidar scan z coordinate : {w_z_obj}")
-        valid_z = np.logical_and((w_z_obj > 0.3), (w_z_obj < 5))
+        valid_z = np.logical_and((w_z_obj > 0.3), (w_z_obj < 2))
 
         #Convert from meters to cells
         x_cell = np.ceil((w_x_obj - self.MAP['xmin']) / self.MAP['res'] ).astype(np.int16)-1 
@@ -125,8 +125,8 @@ class Slam():
             index2 = (output[i][1][:-1])
             self.MAP['log_odds'][index1, index2] -= np.log(4)
         self.MAP['log_odds'][x_cell, y_cell] += np.log(4)
-        self.MAP['log_odds'][self.MAP['log_odds'] < -10] = -10
-        self.MAP['log_odds'][self.MAP['log_odds'] > 10] = 10
+        self.MAP['log_odds'][self.MAP['log_odds'] < -2] = -2
+        self.MAP['log_odds'][self.MAP['log_odds'] > 2] = 2
         self.MAP['map'] = (self.MAP['log_odds'] > 0).astype(np.int8)
         self.MAP['free'] = (self.MAP['log_odds'] < 0).astype(np.int8)
         
@@ -140,9 +140,10 @@ class Slam():
         Bayes filter applied as a particle filter
         N particles, resampling using stratified resampling
         '''
+        np.random.seed(4)
         theta = self.mu[:,2]
-        v = v + np.random.normal(0,0.5, self.N)
-        omega = omega + np.random.normal(0, 0.05, self.N)
+        v = v + np.random.normal(0,0.25, self.N)
+        omega = omega + np.random.normal(0, 0.0005, self.N)
         self.mu[:,0] += np.cos(theta) * v * tau  
         self.mu[:,1] += np.sin(theta) * v * tau 
         self.mu[:,2] += omega * tau 
@@ -158,7 +159,7 @@ class Slam():
         '''
         x,y,theta = self.mu[:,0], self.mu[:,1], self.mu[:,2]
         angles = np.linspace(-5, 185, 286) * np.pi/180
-        valid_lidar = np.logical_and((lidar >= 0.1), (lidar < 40))
+        valid_lidar = np.logical_and((lidar >= 0.1), (lidar < 60))
         ranges = lidar[valid_lidar]
         angles = angles[valid_lidar]
         dx = np.cos(angles) * ranges
@@ -176,7 +177,7 @@ class Slam():
 
         for i in range(self.N): 
             w_x_obj, w_y_obj, w_z_obj = w_s_lidar[i][0],w_s_lidar[i][1],w_s_lidar[i][2]
-            valid_z = np.logical_and((w_z_obj < 5), (w_z_obj > 0.3))
+            valid_z = np.logical_and((w_z_obj < 2), (w_z_obj > 0.3))
             w_x_cell = np.ceil((w_x_obj - self.MAP['xmin'])/self.MAP['res']).astype(np.int16) - 1
             w_y_cell = np.ceil((w_y_obj - self.MAP['ymin'])/self.MAP['res']).astype(np.int16) - 1
             valid_x = np.logical_and((w_x_cell >= 0), (w_x_cell < self.MAP['sizex']))
@@ -189,8 +190,8 @@ class Slam():
             correlation_matrix = pr2_utils.mapCorrelation(self.MAP['map'],self.x_im, self.y_im, Y, self.x_range, self.y_range)
             index = np.unravel_index(np.argmax(correlation_matrix, axis = None), correlation_matrix.shape)
             self.correlation[i] = correlation_matrix[index]
-            self.mu[i,0] += (index[0] - self.x_mid)*self.MAP['res']
-            self.mu[i,1] += (index[1] - self.y_mid)*self.MAP['res']
+            # self.mu[i,0] += (index[0] - self.x_mid)*self.MAP['res']
+            # self.mu[i,1] += (index[1] - self.y_mid)*self.MAP['res']
         
     def resample(self): 
         '''
@@ -219,6 +220,7 @@ class Slam():
         : Control inputs velocity, angular velocity are obtained from the encoder and the FOG sensor data provided in csv files. 
         : Observations are present in the form of lidar scans. 
         '''
+        
         lidar_time, lidar_data = pr2_utils.read_data_from_csv('code/sensor_data/lidar.csv')
         encoder_time, encoder_data = pr2_utils.read_data_from_csv('code/sensor_data/encoder.csv')
         fog_time, fog_data = pr2_utils.read_data_from_csv('code/sensor_data/fog.csv')
@@ -239,19 +241,19 @@ class Slam():
                 vl = np.pi * dl * zl_encoder / (4096 * tau)
                 vr = np.pi * dr * zr_encoder / (4096 * tau)
                 v = (vl + vr) / 2
-                if t_n_fog > len(fog_time) and t_p_fog == t_n_fog:
-                    pass
-                else: 
-                    w = np.sum(fog_data[t_p_fog:t_n_fog , 2]) / (fog_time[t_n_fog] - fog_time[t_p_fog])
+                
+                w = np.sum(fog_data[t_p_fog:t_n_fog , 2]) / (fog_time[t_n_fog] - fog_time[t_p_fog])
                 # print(f"Linear velocity : {v}, Angular velocity : {w}")
                 #Predict step
                 self.predict_step(v, tau,  w)
 
+
                 #Update step
                 if count % 5 == 0:
-                    t_n_lidar = np.abs(t_n_encoder - lidar_time).argmin()
+                    # t_n_lidar = np.abs(t_n_encoder - lidar_time).argmin()
+                    t_n_lidar = min(count, len(lidar_time) - 1)
                     self.update_step(lidar_data[t_n_lidar])
-                    self.alpha *= np.exp(self.correlation)
+                    self.alpha *= np.exp(self.correlation- np.max(self.correlation))
                     self.alpha /= np.sum(self.alpha)
 
                     best_particle = np.argmax(self.correlation)
@@ -269,17 +271,19 @@ class Slam():
                         # print(f"Resampling particles")
                         self.resample()
                     
+                    
                     x_cell = np.ceil((best_mu[0] - self.MAP['xmin']) / self.MAP['res'] ).astype(np.int16)-1
                     y_cell = np.ceil((best_mu[1] - self.MAP['ymin']) / self.MAP['res'] ).astype(np.int16)-1
                     
-                    self.MAP['pose'][count  - 5*k : count,0] = x_cell
-                    self.MAP['pose'][count  - 5*k : count,1] = y_cell
-                    self.MAP['traj'][count  - 5*k : count,0]= best_mu[0]
-                    self.MAP['traj'][count  - 5*k: count,1] = best_mu[1]
-                    self.MAP['traj'][count -5*k : count,2] = best_mu[2]
+                    self.MAP['pose'][count - k*5 : count,0] = x_cell
+                    self.MAP['pose'][count- k*5 : count,1] = y_cell
+                    self.MAP['traj'][count- k*5 : count,0]= best_mu[0]
+                    self.MAP['traj'][count- k*5 : count,1] = best_mu[1]
+                    self.MAP['traj'][count- k*5 : count,2] = best_mu[2]
 
                     if count % 30000 == 0 : 
                         self.show_MAP(count)
+                
         
         with open("map_parameters.pkl", 'wb') as f:
             pickle.dump([self.MAP,self.mu, self.alpha] , f)
@@ -292,7 +296,7 @@ class Slam():
         fig = plt.figure(figsize=(18,6))
         ax1 = fig.add_subplot(121)
         # plt.imshow(self.MAP['map'].T, cmap = "Greys")
-        plt.imshow(~self.MAP['map'].T, cmap = "Greys")
+        plt.imshow(~self.MAP['map'].T, cmap = "hot")
         plt.gca().invert_yaxis()
         plt.title("Occupancy map")
         arrow_properties = dict(
@@ -301,7 +305,7 @@ class Slam():
         ax2 = fig.add_subplot(122)
         plt.scatter(self.MAP['pose'][:,0],self.MAP['pose'][:,1],marker='d', c = 'g',s = 2)
         plt.annotate('Start',c = 'white', fontsize = 'medium', xy = (self.MAP['pose'][1,0], self.MAP['pose'][1,1]), xytext=(self.MAP['pose'][1,0] - 200, self.MAP['pose'][1,1] + 200), arrowprops = arrow_properties)
-        plt.annotate('Finish',c = 'white', fontsize = 'medium', xy = (self.MAP['pose'][-2,0], self.MAP['pose'][-2,1]), xytext=(self.MAP['pose'][-2,0] + 50, self.MAP['pose'][-2,1] + 200), arrowprops = arrow_properties)
+        plt.annotate('Finish',c = 'white', fontsize = 'medium', xy = (self.MAP['pose'][-10,0], self.MAP['pose'][-10,1]), xytext=(self.MAP['pose'][-10,0] + 50, self.MAP['pose'][-10,1] + 200), arrowprops = arrow_properties)
         plt.imshow(self.MAP['free'].T, cmap = "hot")
         plt.gca().invert_yaxis()
         plt.title("Free space map")
@@ -314,6 +318,6 @@ class Slam():
         plt.close()
 
 if __name__ == '__main__':
-    slam = Slam(mode = 2, k = 3)
-    # slam.slam()
+    slam = Slam(mode = 1, k = 3)
+    slam.slam()
     slam.show_MAP(l) 
